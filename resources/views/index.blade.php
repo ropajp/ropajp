0ea117cfc9ea5c4d7a72154b94951c5622ec0fe5
= <=>
2019-12-22 14:07:55 +0900

    deleted type=text/javascript from /view/index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
   <title>{{ config('app.name') }}</title>
 
   <!-- Scripts -->
-  <script src="{{ mix('js/app.js') }}" defer></script>
+  <script src="{{ mix('/js/app.js') }}" defer></script>
 
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
