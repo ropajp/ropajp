8ade98c3afc43e02da58c820098730178cc4bdd8
= <=>
2019-12-22 10:38:13 +0900

    installed vue cli 4.1.1 and runned npm run build

--- webpack.mix.js
+++ webpack.mix.js
@@ -25,3 +25,18 @@ const mix = require('laravel-mix');
 //         startPath: '/'
 //     });
 // }
+
+const path = require('path');
+
+module.exports = {
+  entry: {
+    'index': [
+      path.resolve(__dirname, 'src/index.js')
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+  },
+};
